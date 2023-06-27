c77744967f3e9aea1760571354ce17152a7c3c8c
Anastasiia <anstasiakim2789@gmail.com>
2023-06-27 18:51:15 +0200

    transform password to passwordEncript

--- routerPublicUsers.js
+++ routerPublicUsers.js
@@ -11,12 +11,13 @@ const database= require("./database")
 routerPublicUsers.post("/verification",async(req,res)=>{
     let emailUser = req.body.email  
     let password = req.body.password
-   
+    let cipher = crypto.createCipher(algorithm, keyEncrypt);
+    let passwordEncript = cipher.update(password, 'utf8', 'hex') + cipher.final('hex');
 
     database.connect()
 
     try{
-        const user= await database.query("SELECT * FROM user where email= ? and password= ?", [emailUser, password])
+        const user= await database.query("SELECT * FROM user where email= ? and password= ?", [emailUser, passwordEncript])
         const userEmail= await database.query("SELECT * FROM user where email= ?", [emailUser])
 
         if(user.length>=1 ){
@@ -76,11 +77,11 @@ routerPublicUsers.post("/",async(req,res)=>{
         if(emailUser.length>0){
             database.disConnect()
             return res.send({
-                messege:"email already in use",
+                message:"Email already in use",
                 error: "error in email"
             })
         }else{
-                await database.query("INSERT INTO user (name, password, emai) VALUES  (?, ?, ?) ", [name, passwordEncript,emai])
+                await database.query("INSERT INTO user (name, password, email) VALUES  (?, ?, ?) ", [name, passwordEncript, email])
                 const user= await database.query("SELECT * FROM user where email=? and password=?", [email, passwordEncript])   
                 if(user.length>=1){
         
@@ -96,7 +97,7 @@ routerPublicUsers.post("/",async(req,res)=>{
                     database.disConnect()
                     return res.send(
                     {
-                        messege:"user",
+                        message:"user",
                         apiKey: apiKey,
                         name: user[0].name,
                         userId: user[0].id,
